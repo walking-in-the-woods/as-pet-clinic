0ccf6181683cece4af9e608ef67a643cb79deca4
Alex S <as202009261140@gmail.com>
2021-02-18 15:04:16 +0300

    Refactoring to Common CRUD Interface. Closes #11.

--- pet-clinic-data/src/main/java/as/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/as/petclinic/services/PetService.java
@@ -2,11 +2,6 @@ package as.petclinic.services;
 
 import as.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/as/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/as/petclinic/services/VetService.java
@@ -2,11 +2,6 @@ package as.petclinic.services;
 
 import as.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
